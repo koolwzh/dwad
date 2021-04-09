0585cc87ecfea22d4e4e704ad52b450a44db57b4
koolwzh <78942275+koolwzh@users.noreply.github.com>
2021-04-08 18:57:59 -0600

    Create bob.py

---
+++ bob.py
@@ -0,0 +1,14 @@
+bob = int(input("enter a number"))
+b = ["57234" "00907"]
+h = []
+h.append(bob)
+ff = (h[2:5]) 
+df = (h[1:1])
+d3f = (h[2:2])
+sum3 = (d3f) + (df)
+if (sum3 % 2 == 0):
+  print("right") 
+  print(ff) 
+else:  
+  print("left")
+  print(ff)
